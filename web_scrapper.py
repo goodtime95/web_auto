0355ad07094efda2c5dcdc3065569a78fdff56e5
Victor Bontemps <Victor.Bontemps@elevacapital.com>
2025-08-29 10:48:32 +0200

    Tutorial Video Playwright Web scrapping + CAPTCHA Bypass tuto

--- web_scrapper.py
+++ web_scrapper.py
@@ -5,3 +5,14 @@ Created on Fri Aug 29 09:39:14 2025
 @author: Victor.Bontemps
 """
 
+from playwright.sync_api import sync_playwright
+
+pw = sync_playwright().start()
+browser = pw.firefox.launch(
+    headless=False, slow_mo=2000)
+page = browser.new_page()
+page.goto("http://google.com")
+print(page.content())
+print(page.title())
+page.screenshot(path="example.png")
+browser.close()
